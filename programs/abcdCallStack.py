ca1903051f0754fc289a86291e7a059923502038
Anch0r-Lad <kj.siharath@gmail.com>
2022-04-09 21:25:03 +0000

    chapter 4

--- programs/abcdCallStack.py
+++ programs/abcdCallStack.py
@@ -2,19 +2,20 @@ def a():
     print ('a() starts')
     b()
     d()
-print ('a() returns')
+    print ('a() returns')
 
 def b():
     print ('b() starts')
     c()
-print('b() returns')
+
+    print('b() returns')
 
 def c():
     print('c() starts')
-print('c() returns')
+    print('c() returns')
 
 def d():
     print('d() starts')
-print('d() returns')
+    print('d() returns')
 
 a()
--- programs/sameNameLocalGlobal.py
+++ programs/sameNameLocalGlobal.py
@@ -3,11 +3,11 @@ def spam():
     eggs ='spam'#this is the global
 
 def bacon():
-    eggs = 'bacon' # this is a local
+    return 'crispy bacon' # this is a local
 
 def ham():
     print(eggs) #this is the global
 
 eggs = 42 # this is the global
-spam()
+eggs = bacon()
 print(eggs)
